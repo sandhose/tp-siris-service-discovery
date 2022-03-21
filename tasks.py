03c4a277397469dd5ee890d2a2958827856ba834
Quentin Gliech <quentingliech@gmail.com>
2022-03-21 23:35:45 +0100

    Include node hostname in API result

--- tasks.py
+++ tasks.py
@@ -1,15 +1,27 @@
-from typing import Optional
+import platform
+from typing import Optional, TypedDict
 import requests
 from celery import Celery
 from bs4 import BeautifulSoup
 
 app = Celery(__name__, config_source="config")
 
+class Result(TypedDict):
+    node: str
+    title: Optional[str]
+    url: str
+
 @app.task
-def get_title(url: str) -> Optional[str]:
+def get_title(url: str) -> Result:
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    title = soup.find('title')
-    if title:
-        return title.get_text()
-    return None
+    element = soup.find('title')
+    title = None
+    if element:
+        title = element.get_text()
+
+    return {
+        'node': platform.node(),
+        'title': title,
+        'url': url
+    }
